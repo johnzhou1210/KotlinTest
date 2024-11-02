bf2383e4773a420965c88c2a91d3dc25cf31e673
John Zhou <chowjohn10@gmail.com>
2024-11-02 02:29:31 -0400

    haptic feedback and constraints

--- app/src/main/java/com/example/kotlintest/Counter.kt
+++ app/src/main/java/com/example/kotlintest/Counter.kt
@@ -1,15 +1,18 @@
 package com.example.kotlintest
 
+import android.content.Context
+import android.os.Vibrator
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -20,54 +23,86 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+
 
 @Composable
 fun Counter(innerPadding: PaddingValues) {
     var counter by remember { mutableIntStateOf(0) }
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (counterText, buttons) = createRefs()
+        val context = LocalContext.current
 
-    Column {
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Row(
+        Box(
             modifier = Modifier
                 .padding(innerPadding)
-                .wrapContentSize()
-                .align(Alignment.CenterHorizontally)
-        ) {
+                .fillMaxWidth()
+                .constrainAs(counterText) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
 
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Spacer(modifier = Modifier.height(500.dp))
             Text(
                 text = "$counter",
-                style = MaterialTheme.typography.headlineLarge,
-                fontSize = 256.sp,
-                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineLarge.copy(fontSize = 256.sp),
+                modifier = Modifier,
+                fontSize = if (counter.toString().length <= 2) 256.sp else if (counter.toString().length <= 3) 192.sp else 128.sp,
+
                 fontFamily = FontFamily.Monospace,
+                maxLines = 1,
             )
         }
 
-        Row(
-            modifier = Modifier
+        Column(modifier = Modifier.constrainAs(buttons) {
+            bottom.linkTo(parent.bottom)
+            top.linkTo(parent.baseline)
 
-                .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            ResetButton { counter = 0 }
+        }) {
+            Spacer(modifier = Modifier.height(480.dp))
+
+            Row(
+                modifier = Modifier
+
+                    .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ResetButton {
+                    counter = 0
+                    vibrate(context)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(220.dp))
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 25.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                DecrementButton {
+                    counter -= 1
+                    vibrate(context)
+                }
+                Spacer(modifier = Modifier.width(32.dp))
+                IncrementButton {
+                    counter += 1
+                    vibrate(context)
+                }
+            }
+
         }
 
-        Spacer(modifier = Modifier.height(220.dp))
-
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 25.dp)
-                .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            DecrementButton { counter -= 1 }
-            Spacer(modifier = Modifier.width(32.dp))
-            IncrementButton { counter += 1 }
-        }
 
     }
+
+
 }
 
 
@@ -105,4 +140,10 @@ fun ResetButton(onReset: () -> Unit) {
     ) {
         Text("Reset", fontSize = 16.sp, fontFamily = FontFamily.Monospace)
     }
+}
+
+
+fun vibrate(context: Context) {
+    val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    vibrator.vibrate(50)
 }
